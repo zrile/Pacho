6a59f9dd1c4c7e6a08b1e6fd5bfad3b07cc961df
Zrinka Ratkovic <zrinka.ratkovic@gmail.com>
2023-11-22 07:16:56 +0100

    prvi

---
+++ main.py
@@ -0,0 +1,10 @@
+import docx
+doc = docx.Document('probno.docx')
+print (len(doc.paragraphs))
+for i in range (len(doc.paragraphs)):
+    print (doc.paragraphs[i].text)
+
+for table in doc.tables:
+    for row in table.rows:
+        for cell in row.cells:
+            print (cell.text)
